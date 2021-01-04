e03f68f041100ffb481d8b82558cd3b2bc9fe19c
diego dorado <diegodorado78@gmail.com>
2021-01-04 18:37:23 -0500

    problem fixed

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from './MediaPlayer.ts';
-import AutoPlay from './plugins/AutoPlay.ts';
-import AutoPause from './plugins/AutoPause.ts';
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
 
 //mdn html media element para ver props, metodos de los medios
 const video:HTMLMediaElement = document.querySelector("video");
